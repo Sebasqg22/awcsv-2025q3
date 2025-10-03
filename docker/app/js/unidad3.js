18591cf2a14a47dfacf68e52544fbe286294ca1a
klealufide <kleal40321@ufide.ac.cr>
2025-10-02 19:06:22 -0600

    Base Javascript

---
+++ docker/app/js/unidad3.js
@@ -0,0 +1,13 @@
+console.log("Hello world!");
+
+var name = "Karol";
+let lastname = "Leal";
+let age = 35;
+let isStudent = false;
+let salary = 100.89;
+
+console.log(name);
+console.log(lastname);
+console.log(age);
+console.log(isStudent);
+console.log(salary);
