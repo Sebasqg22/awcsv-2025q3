03482a23a0e4ebf7b1ca05bf79cb6f03c5fc1853
klealufide <kleal40321@ufide.ac.cr>
2025-10-09 20:28:50 -0600

    Unidad 3 completa

--- docker/app/js/unidad3.js
+++ docker/app/js/unidad3.js
@@ -88,3 +88,82 @@ switch (color) {
         console.log("no esta definido!");
         break;
 }
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+console.log(listaFrutas[1]);
+for (let x in listaFrutas) {
+    console.log(listaFrutas[x]);
+}
+
+for (let i = 0; i <= 10; i++) {
+    console.log("5 x " + i + "=" + (i * 5));
+}
+
+edad = 45;
+
+do { // ejecuta al menos una vez el bloque de codigo, sin importar la condicion
+    edad++;
+    console.log("dowhile", edad);
+} while (edad < 30); //  ve la condicion y si true vuelve ejecutar y si no sale de la  iteracion
+console.log("Salio con edad", edad);
+
+edad = 45;
+while (edad < 30) { // ve la condicion si cumple ejecuta y si no tan siquiera entra
+    edad++;
+    console.log("while", edad);
+}
+
+function sumar(a, b) {
+    return a + b;
+}
+
+function restar(a, b) {
+    return a - b;
+}
+
+function saludar(nombre) {
+    console.log("Hola! " + nombre)
+}
+let numero1 = 6;
+let numero2 = 3;
+let operador = "*";
+let total_calculadora = 0;
+switch (operador) {
+    case "+":
+        total_calculadora = sumar(numero1, numero2);
+        break;
+}
+console.log(total_calculadora);
+saludar("Karol");
+
+document.addEventListener("DOMContentLoaded", function () { // me aseguro que el DOM este cargado
+    document.getElementById("mensaje").style.display = "none";
+    document.getElementById("titulo").innerText = "Unidad 3";
+    //document.getElementById("actividades").innerHTML = "<h2>Actividades</h2>";
+    let nuevoLi = document.createElement("li");
+    nuevoLi.innerText = "Conferencias";
+    document.getElementById("listaActividades").appendChild(nuevoLi);
+
+    document.getElementById("actividades").style.backgroundColor = "orange";
+    document.getElementById("listaActividades").style.backgroundColor = "orange";
+    document.getElementById("actividad").style.backgroundColor = "white";
+    document.getElementById("actividad").style.borderColor = "violet";
+
+    document.getElementById("agregarActividades").addEventListener("click", function () {
+        let actividad = document.getElementById("actividad").value;
+        if (actividad != "") {
+            let nuevoLi = document.createElement("li");
+            nuevoLi.innerText = actividad;
+            document.getElementById("listaActividades").appendChild(nuevoLi);
+            document.getElementById("actividad").value = "";
+            document.getElementById("actividad").style.backgroundColor = "white";
+            document.getElementById("mensaje").style.display = "none";
+        } else {
+            document.getElementById("actividad").style.backgroundColor = "red";
+            document.getElementById("mensaje").style.display = "block";
+        }
+        console.log(actividad);
+    })
+})
